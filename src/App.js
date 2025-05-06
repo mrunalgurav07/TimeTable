e4752f2ad67748c39dc97fa39306f85676742c66
Pranav Gavandi <pranavgavandi4466@gmail.com>
2025-05-06 23:56:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './LoginPage/LoginPage';
 import TimeTable from './TimeTable/TimeTable';
 
